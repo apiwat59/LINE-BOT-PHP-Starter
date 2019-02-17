55d89370b11588cfed046e52993d055845184afd
apiwat59 <32583026+apiwat59@users.noreply.github.com>
2019-02-17 09:11:01 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,20 +1,20 @@
 <?php
-$access_token = 'iHrKTePlPqILV/X59sGJhzuJ0pkWAm/lZrjsG6/FbioFScroH42u6LkD9D5Wg29Qm3y45yTVZMJr4Cv5OBDDMymYk16SCazYbZLBWKoqroZW3YbDFZJSQcNdbkvgFjWaUmGvLocijk9KvaklfYmKsAdB04t89/1O/w1cDnyilFU=';
-$proxy = 'velodrome.usefixie.com:80';
-$proxyauth = 'fixie:U0hIWNH9iS4zo5W';
+$access_token = 'mBMq4+bnv+GRs4j+OZq71Hd86b539QhX9fDhf0aME1j+aWe73P/bml5eGNnrCC631NVOe4W10DF9CPk0pIAIoU4jtCaKmqcN+9wlCGyT758C8HEpZZ4m6vwR+jobXHYxOlKuJV2qKS2p3aOZDMTC6wdB04t89/1O/w1cDnyilFU=
+';
+
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
 $headers = array('Authorization: Bearer ' . $access_token);
 
+
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_PROXY, $proxy);
-curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
 $result = curl_exec($ch);
 curl_close($ch);
+echo $result;
 
 echo $result;
 ?>
