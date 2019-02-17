9722a7e5caa6fb200ddb5cceb1fad4e70534fa08
apiwat59 <32583026+apiwat59@users.noreply.github.com>
2019-02-17 19:49:36 +0700

    Update bot_settings.php

--- bot_settings.php
+++ bot_settings.php
@@ -1,6 +1,7 @@
 <?php
 /// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
-define('LINE_MESSAGE_CHANNEL_ID','กรอก ค่า Channel ID');
-define('LINE_MESSAGE_CHANNEL_SECRET','กรอกค่า Channel secret');
-define('LINE_MESSAGE_ACCESS_TOKEN','กรอกค่า Channel access token');
+define('LINE_MESSAGE_CHANNEL_ID','1645912420');
+define('LINE_MESSAGE_CHANNEL_SECRET','8867bf01d790c4a0e3d97ae7ac9caa5d');
+define('LINE_MESSAGE_ACCESS_TOKEN','mBMq4+bnv+GRs4j+OZq71Hd86b539QhX9fDhf0aME1j+aWe73P/bml5eGNnrCC631NVOe4W10DF9CPk0pIAIoU4jtCaKmqcN+9wlCGyT758C8HEpZZ4m6vwR+jobXHYxOlKuJV2qKS2p3aOZDMTC6wdB04t89/1O/w1cDnyilFU=
+');
 ?>
